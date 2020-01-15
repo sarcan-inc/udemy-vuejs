4b7c52a250d94e895d4e9475c144b2c6e1608f83
sarcan-inc <59588553+sarcan-inc@users.noreply.github.com>
2020-01-15 12:19:21 +0900

    aaa
    ああああ

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import CommonHeader from './components/CommonHeader.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import CommonHeader from "./components/CommonHeader.vue";
 
-Vue.config.productionTip = false
-Vue.component("CommonHeader",CommonHeader);
-
+Vue.config.productionTip = false;
+Vue.component("CommonHeader", CommonHeader);
+Vue.filter("upperCase", function(val) {
+  return val.toUpperCase();
+});
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
